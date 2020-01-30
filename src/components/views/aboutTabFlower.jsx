6ac2f4cfa84c1c514f471e2b9dac6089e83c5b10
Njugen <privat_thai_nguyen@hotmail.com>
2020-01-30 18:31:33 +0200

    Added simple information about what Tabflower is to its about page

--- src/components/views/aboutTabFlower.jsx
+++ src/components/views/aboutTabFlower.jsx
@@ -5,8 +5,18 @@ class AboutTabFlowerView extends View {
     render = () => {
         return (
             <div className="row">
-                <div className="col-12">
-                    <h2>{this.props.label ? this.props.label : "About Tabflower"}</h2>
+                <div className="col-6">
+                    <h3>{this.props.label ? this.props.label : "About Tabflower"}</h3>
+                    <p>
+                        Tabflower is a webextension for managing browser windows and tabs. It is intended for people who have
+                        problems keeping their browser and all its tabs - and therefore resource consumption - in check. Tabflower is also
+                        intended for people who - for whatever reason - needs to schedule or group their activities on the web, as it offers
+                        the tools to do so. 
+                    </p>
+                    <p>
+                        Tabflower is created by me, Thai Nguyen, and is under constant development. If you have found bugs, or have somekind of 
+                        feedback (e.g. suggestions for new features), please contact me at <a href="mailto:privat_thai_nguyen@hotmail.com">privat_thai_nguyen@hotmail.com</a>
+                    </p>
                 </div>
             </div>
         );
