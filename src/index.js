6c361eaf937d055107c36cafdae355ea73fb40fe
Njugen <privat_thai_nguyen@hotmail.com>
2020-02-03 13:42:03 +0200

    Changed <BrowserRouter> to <HashRouter> in index.js as a workaround for the path reload issuecaused by reloading paths in web browsers after installing the build as a unpacked extension. Read more: https://github.com/Njugen/tabflower/issues/9

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './styles/react-generated/index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<HashRouter basename="index.html" hashType="noslash"><App /></HashRouter>, document.getElementById('root'));
+ReactDOM.render(<HashRouter hashType="noslash"><App /></HashRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
