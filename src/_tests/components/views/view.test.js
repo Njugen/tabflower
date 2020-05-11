a9ce64b5314bf47f743cb9ad17ec4b65c026774b
Njugen <privat_thai_nguyen@hotmail.com>
2020-05-11 13:39:45 +0300

    added view.test.js with a very, very basic template

---
+++ src/_tests/components/views/view.test.js
@@ -0,0 +1,56 @@
+import React, { Fragment } from 'react';
+import { shallow, mount, render } from 'enzyme';
+import Modal from './../../../components/modals/modal';
+import * as ExceptionsHandler from './../../../components/utils/exceptionsAndHandler';
+import * as validator from './../../../components/utils/inputValidators';
+
+
+const predefinedComponent = (props, options) => {
+    props = props || {};
+
+    const component = shallow(<Modal {...props} />, options);
+    return component;
+}
+
+const presetProps = {
+    onRaiseToErrorOverlay: "",
+    onRaiseToModal: "",
+    onViewMount: ""
+};
+
+let testComponent;
+let componentInstance;
+
+describe("Test <View /> component behaviour at mount", () => {
+    
+
+    const actualErrorReturns = {
+        "mp-verifyProps-101": ExceptionsHandler.ValidatorError("mp-verifyProps-101"),
+        
+    };
+
+    const expectedErrorReturns = {
+        "mp-verifyProps-101": {
+            name: "ValidatorError",
+            message: "The \"onDismiss\" props function is missing in the requested modal. Request aborted.",
+            code: "mp-verifyProps-101"
+        }
+        
+    }
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+        jest.useRealTimers();
+
+        const presetProps = {};
+        testComponent = predefinedComponent(presetProps, { disableLifecycleMethods: true });
+        componentInstance = testComponent.instance();
+    
+        ExceptionsHandler.ValidatorError = jest.fn();
+        ExceptionsHandler.ValidatorError.mockImplementation(errCode => {
+            return actualErrorReturns[errCode];
+        });
+    });
+
+    
+}) 
