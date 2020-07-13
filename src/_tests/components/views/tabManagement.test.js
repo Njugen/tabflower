a28a855535756bd3b0d5b754d75e899b312e6c6f
Njugen <privat_thai_nguyen@hotmail.com>
2020-07-13 17:04:32 +0300

    reviewed and rewrote view.test.js and tabManagement.test.js

--- src/_tests/components/views/tabManagement.test.js
+++ src/_tests/components/views/tabManagement.test.js
@@ -1,111 +1,122 @@
-import React, { Fragment } from 'react';
-import { shallow, mount, render } from 'enzyme';
-import TabManagementView from './../../../components/views/tabManagement';
-import * as ExceptionsHandler from './../../../components/utils/exceptionsAndHandler';
-import * as validator from './../../../components/utils/inputValidators';
-
+import React, { Fragment } from "react";
+import { shallow, mount, render } from "enzyme";
+import TabManagementView from "./../../../components/views/tabManagement";
+import * as ExceptionsHandler from "./../../../components/utils/exceptionsAndHandler";
+import * as validator from "./../../../components/utils/inputValidators";
 
 const predefinedComponent = (props, options) => {
-    props = props || {};
+  props = props || {};
 
-    const component = shallow(<TabManagementView {...props} />, options);
-    return component;
-}
-
-const presetProps = {
-    onRaiseToErrorOverlay: "",
-    onRaiseToModal: "",
-    onViewMount: ""
+  const component = shallow(<TabManagementView {...props} />, options);
+  return component;
 };
+
+const presetProps = {};
 
 let testComponent;
 let componentInstance;
 
 describe("Test <TabManagementView /> component behaviour at mount", () => {
-    
+  const actualErrorReturns = {
+    "tabManagement-view-101": ExceptionsHandler.ValidatorError(
+      "tabManagement-view-101"
+    ),
+    "tabManagement-view-102": ExceptionsHandler.ValidatorError(
+      "tabManagement-view-102"
+    ),
+  };
 
-    const actualErrorReturns = {
-        "tabManagement-view-101": ExceptionsHandler.ValidatorError("tabManagement-view-101"),
-        "tabManagement-view-102": ExceptionsHandler.ValidatorError("tabManagement-view-102")
-    };
+  const expectedErrorReturns = {
+    "tabManagement-view-101": {
+      name: "ValidatorError",
+      message:
+        "An error has occured when attempting to reload the user interface. However, the requested data changes have been made successfully. Please, reload this page manually to see these changes.",
+      code: "tabManagement-view-101",
+    },
+    "tabManagement-view-102": {
+      name: "ValidatorError",
+      message:
+        "The command raised to the TabManagementView component could not be executed, because it is invalid.",
+      code: "tabManagement-view-102",
+    },
+  };
 
-    const expectedErrorReturns = {
-        "tabManagement-view-101": {
-            name: "ValidatorError",
-            message: "An error has occured when attempting to reload the user interface. However, the requested data changes have been made successfully. Please, reload this page manually to see these changes.",
-            code: "tabManagement-view-101"
-        },
-        "tabManagement-view-102": {
-            name: "ValidatorError",
-            message: "The command raised to the TabManagementView component could not be executed, because it is invalid.",
-            code: "tabManagement-view-102"
+  beforeEach(() => {
+    jest.clearAllMocks();
+    jest.useRealTimers();
+
+    const presetProps = {};
+    testComponent = predefinedComponent(presetProps, {
+      disableLifecycleMethods: true,
+    });
+    componentInstance = testComponent.instance();
+
+    ExceptionsHandler.ErrorHandler = jest.fn();
+    ExceptionsHandler.ValidatorError = jest.fn();
+    ExceptionsHandler.ValidatorError.mockImplementation((errCode) => {
+      return actualErrorReturns[errCode];
+    });
+  });
+
+  describe("test handleRaiseToView(data)", () => {
+    describe('Case 1: When "data" is NOT a string', () => {
+      const various_nonString_values = [
+        [13],
+        [null],
+        [undefined],
+        [true],
+        [false],
+        [() => {}],
+        [{ items: 0 }],
+        [["a", "b", "c"]],
+      ];
+
+      test('Run handleRaiseToView(): The error ExceptionsHandler.ValidatorError("tabManagement-view-101") should be thrown', () => {
+        componentInstance.handleRaiseToView();
+
+        expect(ExceptionsHandler.ValidatorError).toHaveBeenCalledWith(
+          "tabManagement-view-101"
+        );
+      });
+
+      test.each(various_nonString_values)(
+        'Run handleRaiseToView(%p): The error ExceptionsHandler.ValidatorError("tabManagement-view-101") should be thrown',
+        (val) => {
+          componentInstance.handleRaiseToView(val);
+
+          expect(ExceptionsHandler.ValidatorError).toHaveBeenCalledWith(
+            "tabManagement-view-101"
+          );
         }
-    } 
-
-    beforeEach(() => {
-        jest.clearAllMocks();
-        jest.useRealTimers();
-
-        const presetProps = {};
-        testComponent = predefinedComponent(presetProps, { disableLifecycleMethods: true });
-        componentInstance = testComponent.instance();
-        
-        ExceptionsHandler.ErrorHandler = jest.fn();
-        ExceptionsHandler.ValidatorError = jest.fn();
-        ExceptionsHandler.ValidatorError.mockImplementation(errCode => {
-            return actualErrorReturns[errCode];
-        });
+      );
     });
 
-    describe("test handleRaisedData(data)", () => {
-        describe("Case 1: When \"data\" is NOT a string", () => {
-            const various_data = [
-                [13],
-                [null],
-                [undefined],
-                [true],
-                [false],
-                [() => {}],
-                [{items: 0}],
-                [["a", "b", "c"]]
-            ];
+    describe('Case 2: When "data" is a string, but the string is invalid (invalid means data != "refresh")', () => {
+      test('Run handleRaiseToView("buyMeSweets"): The error The error ExceptionsHandler.ValidatorError("tabManagement-view-102") should b e thrown', () => {
+        componentInstance.handleRaiseToView("buyMeSweets");
 
-            test("Run handleRaisedData(): The error ExceptionsHandler.ValidatorError(\"tabManagement-view-101\") should be thrown", () => {
-                componentInstance.handleRaisedData();
+        expect(ExceptionsHandler.ValidatorError).toHaveBeenCalledWith(
+          "tabManagement-view-102"
+        );
+      });
+    });
 
-                expect(ExceptionsHandler.ValidatorError).toHaveBeenCalledWith("tabManagement-view-101");
-            });
+    describe('Case 3: When "data" is a string AND the string is valid (valid means data = "refresh")', () => {
+      test('Run handleRaiseToView("refresh"): The this.refreshView() function should be called', () => {
+        componentInstance.refreshView = jest.fn();
+        componentInstance.handleRaiseToView("refresh");
 
-            test.each(various_data)("Run handleRaisedData(%p): The error ExceptionsHandler.ValidatorError(\"tabManagement-view-101\") should be thrown", (val) => {
-                componentInstance.handleRaisedData(val);
+        expect(componentInstance.refreshView).toHaveBeenCalled();
+      });
 
-                expect(ExceptionsHandler.ValidatorError).toHaveBeenCalledWith("tabManagement-view-101");
-            });
-        });
+      test('Run handleRaiseToView("refresh"): The component\'s refresh factor should increase by one', () => {
+        const initialRefreshFactor = componentInstance.state.refreshFactor;
+        componentInstance.handleRaiseToView("refresh");
 
-        describe("Case 2: When \"data\" is a string AND the string is valid", () => {
-            test("Run handleRaisedData(\"refresh\"): The this.setState() function should be called", () => {
-                componentInstance.setState = jest.fn();
-                componentInstance.handleRaisedData("refresh");
-
-                expect(componentInstance.setState).toHaveBeenCalled();
-            });
-
-            test("Run handleRaisedData(\"refresh\"): The component's refresh factor should increase by one", () => {
-                const initialRefreshFactor = componentInstance.state.refreshFactor;
-                componentInstance.handleRaisedData("refresh");
-
-                expect(componentInstance.state.refreshFactor).toBe(initialRefreshFactor + 1);
-            });
-        });
-
-        describe("Case 3: When \"data\" is a string, but the string is invalid", () => {
-            test("Run handleRaisedData(\"buyMeSweets\"): The this.setState() function should be called", () => {
-                componentInstance.handleRaisedData("buyMeSweets");
-
-                expect(ExceptionsHandler.ValidatorError).toHaveBeenCalledWith("tabManagement-view-102");
-            });
-
-        });
-    })
-}) 
+        expect(componentInstance.state.refreshFactor).toBe(
+          initialRefreshFactor + 1
+        );
+      });
+    });
+  });
+});
--- src/_tests/components/views/view.test.js
+++ src/_tests/components/views/view.test.js
@@ -1,295 +1,178 @@
-import React, { Fragment } from 'react';
-import { shallow, mount, render } from 'enzyme';
-import View from './../../../components/views/view';
-import * as ExceptionsHandler from './../../../components/utils/exceptionsAndHandler';
-import * as validator from './../../../components/utils/inputValidators';
-
-
+import React from "react";
+import { shallow } from "enzyme";
+import View from "./../../../components/views/view";
+import * as ExceptionsHandler from "./../../../components/utils/exceptionsAndHandler";
 
 const predefinedComponent = (props, options) => {
-    props = props || {};
+  props = props || {};
 
-    const component = shallow(<View {...props} />, options);
-    return component;
-}
+  const component = shallow(<View {...props} />, options);
+  return component;
+};
 
 const presetProps = {
-    onRaiseToErrorOverlay: "",
-    onRaiseToModal: "",
-    onViewMount: ""
+  onViewMount: "",
 };
 
 let testComponent;
 let componentInstance;
 
 describe("Test <View /> component behaviour at mount", () => {
-    
+  const actualErrorReturns = {
+    "view-101": ExceptionsHandler.ValidatorError("view-101"),
+    "view-102": ExceptionsHandler.ValidatorError("view-102"),
+    "view-103": ExceptionsHandler.ValidatorError("view-103"),
+    "view-104": ExceptionsHandler.ValidatorError("view-104"),
+  };
 
-    const actualErrorReturns = {
-        "view-101": ExceptionsHandler.ValidatorError("view-101"),
-        "view-102": ExceptionsHandler.ValidatorError("view-102"),
-        "view-103": ExceptionsHandler.ValidatorError("view-103"),
-        "view-104": ExceptionsHandler.ValidatorError("view-104")
-    };
+  const expectedErrorReturns = {
+    "view-101": {
+      name: "ValidatorError",
+      message:
+        'The "onViewMount" parameter is not set in this view. Any view inserted into Tabflower must have this parameter set as a function.',
+      code: "view-101",
+    },
+    "view-102": {
+      name: "ValidatorError",
+      message:
+        'The "data" parameter is not set in raiseToErrorOverlay() of the View component. Information could not be forwarded to the UI.',
+      code: "view-102",
+    },
+    "view-103": {
+      name: "ValidatorError",
+      message:
+        "The features of the raiseToErrorOverlay() function of the View component could not be fully executed, because the props onRaiseToErrorOverlay is not a function or is missing.",
+      code: "view-103",
+    },
+    "view-104": {
+      name: "ValidatorError",
+      message:
+        'The "data" parameter is not set as an object in raiseToModal() of the View component. Information could not be forwarded to the UI.',
+      code: "view-104",
+    },
+  };
 
-    const expectedErrorReturns = {
-        "view-101": {
-            name: "ValidatorError",
-            message: "The \"onViewMount\" parameter is not set in this view. Any view inserted into Tabflower must have this parameter set as a function.",
-            code: "view-101"
-        },
-        "view-102": {
-            name: "ValidatorError",
-            message: "The \"data\" parameter is not set in raiseToErrorOverlay() of the View component. Information could not be forwarded to the UI.",
-            code: "view-102"
-        },
-        "view-103": {
-            name: "ValidatorError",
-            message: "The features of the raiseToErrorOverlay() function of the View component could not be fully executed, because the props onRaiseToErrorOverlay is not a function or is missing.",
-            code: "view-103"
-        },
-        "view-104": {
-            name: "ValidatorError",
-            message: "The \"data\" parameter is not set as an object in raiseToModal() of the View component. Information could not be forwarded to the UI.",
-            code: "view-104"
-        }
-    }
+  beforeEach(() => {
+    jest.clearAllMocks();
+    jest.useRealTimers();
 
-    beforeEach(() => {
-        jest.clearAllMocks();
-        jest.useRealTimers();
+    const presetProps = {};
+    testComponent = predefinedComponent(presetProps, {
+      disableLifecycleMethods: true,
+    });
+    componentInstance = testComponent.instance();
 
-        const presetProps = {};
-        testComponent = predefinedComponent(presetProps, { disableLifecycleMethods: true });
+    ExceptionsHandler.ValidatorError = jest.fn();
+    ExceptionsHandler.ValidatorError.mockImplementation((errCode) => {
+      return actualErrorReturns[errCode];
+    });
+  });
+
+  describe("Test handleViewMount()", () => {
+    const various_props_onViewMount = [
+      ["A string representing a dummy data variable"], // string
+      [32], // number
+      [null], // null
+      [undefined], // undefined
+      [false], // boolean
+      [true], // boolean
+      [[12, 8, 3, 7]], // array
+      [{ item: "test" }], //
+    ];
+
+    test.each(various_props_onViewMount)(
+      'Run handleViewMount(), when this.props.onViewMount = %p. Expectation: Throw an error ExceptionsHandler.ValidatorError("view-101")',
+      (val) => {
+        const presetProps = {
+          onViewMount: val,
+        };
+        testComponent = predefinedComponent(presetProps, {
+          disableLifecycleMethods: true,
+        });
         componentInstance = testComponent.instance();
-    
-        ExceptionsHandler.ValidatorError = jest.fn();
-        ExceptionsHandler.ValidatorError.mockImplementation(errCode => {
-            return actualErrorReturns[errCode];
-        });
+
+        componentInstance.handleViewMount();
+        expect(ExceptionsHandler.ValidatorError).toHaveBeenCalledWith(
+          "view-101"
+        );
+      }
+    );
+
+    test("Run handleViewMount(), when this.props.onViewMount is a function: Call this.props.onViewMount() with the component state as input parameter", () => {
+      const presetProps = {
+        onViewMount: jest.fn(),
+      };
+      testComponent = predefinedComponent(presetProps, {
+        disableLifecycleMethods: true,
+      });
+      componentInstance = testComponent.instance();
+
+      componentInstance.handleViewMount();
+      expect(componentInstance.props.onViewMount).toHaveBeenCalledWith(
+        componentInstance.state
+      );
+    });
+  });
+
+  describe("Test componentDidMount()", () => {
+    const various_nonFunction_values = [
+      ["A string representing a dummy data variable"], // string
+      [32], // number
+      [null], // null
+      [undefined], // undefined
+      [false], // boolean
+      [true], // boolean
+      [[12, 8, 3, 7]], // array
+      [{ item: "test" }], // object
+    ];
+
+    const various_values_allDatatypes = [
+      ...various_nonFunction_values,
+      [() => {}],
+    ];
+
+    test.each(various_nonFunction_values)(
+      "Run componentDidMount(), when this.childComponentDidMount = %p (not a function): Do not trigger this.childComponentDidMount() (if this test fails, it means error has been thrown)",
+      (val) => {
+        componentInstance.childComponentDidMount = val;
+
+        expect(() => {
+          componentInstance.componentDidMount();
+        }).not.toThrowError();
+      }
+    );
+
+    test("Run componentDidMount, when this.childComponentDidMount is a function: Call this.childComponentDidMount()", () => {
+      componentInstance.childComponentDidMount = jest.fn();
+      componentInstance.componentDidMount();
+
+      expect(componentInstance.childComponentDidMount).toHaveBeenCalled();
     });
 
-    describe("Test raiseToErrorOverlay(data)", () => {
-        const various_data = [
-            ["A string representing a dummy data variable"],
-            [32],
-            [null],
-            [undefined],
-            [false],
-            [true],
-            [[12,8,3,7]],
-            [() => {}]
-        ];
+    describe("Test componentDidMount, when this.childComponentDidMount has any value. this.handleViewMount() should trigger at all time.", () => {
+      test.each(various_values_allDatatypes)(
+        "Run componentDidMount(), when this.childComponentDidMount = %p. this.handleViewMount() should trigger",
+        (val) => {
+          componentInstance.handleViewMount = jest.fn();
+          componentInstance.childComponentDidMount = val;
+          componentInstance.componentDidMount();
 
-        describe("Case 1: When the \"data\" parameter is NOT an object", () => {
-            test("Run raiseToErrorOverlay(): Throw an error ExceptionsHandler.ValidatorError(\"view-102\")", () => {
-                componentInstance.raiseToErrorOverlay();
+          expect(componentInstance.handleViewMount).toHaveBeenCalled();
+        }
+      );
 
-                expect(ExceptionsHandler.ValidatorError).toHaveBeenCalledWith("view-102");
-            });
+      test("Run componentDidMount(), with no regards to this.childComponentDidMount. this.handleViewMoun() should still trigger", () => {
+        componentInstance.handleViewMount = jest.fn();
+        componentInstance.componentDidMount();
 
-            test.each(various_data)("Run raiseToErrorOverlay(%p): Throw an error ExceptionsHandler.ValidatorError(\"view-102\")", (val) => {
-                componentInstance.raiseToErrorOverlay(val);
-
-                expect(ExceptionsHandler.ValidatorError).toHaveBeenCalledWith("view-102");
-            });
-        });
-
-        describe("Case 2: When the \"data\" parameter is an object, but the onRaiseToErrorOverlay props is not a function", () => {
-            const various_props_onRaiseToErrorOverlay = [
-                ["A string representing a dummy data variable"],
-                [32],
-                [null],
-                [undefined],
-                [false],
-                [true],
-                [[12,8,3,7]],
-                [{ testItem: "nothing here" }]
-            ];
-            
-            test.each(various_props_onRaiseToErrorOverlay)("Run raiseToErrorOverlay({ testData: \"test value\" }) when this.props.onRaiseToErrorOverlay = %p: Throw an error ExceptionsHandler.ValidatorError(\"view-103\")", (val) => {
-                const presetProps = {
-                    onRaiseToErrorOverlay: val
-                };
-                testComponent = predefinedComponent(presetProps, { disableLifecycleMethods: true });
-                componentInstance = testComponent.instance();
-
-                const data_param = { testData: "test value" }
-
-                componentInstance.raiseToErrorOverlay(data_param);
-
-                expect(ExceptionsHandler.ValidatorError).toHaveBeenCalledWith("view-103");
-            });
-
-            test("Run raiseToErrorOverlay({ testData: \"test value\" }) when this.props.onRaiseToErrorOverlay does not exist: Throw an error ExceptionsHandler.ValidatorError(\"view-103\")", () => {
-                const presetProps = {
-              
-                };
-                testComponent = predefinedComponent(presetProps, { disableLifecycleMethods: true });
-                componentInstance = testComponent.instance();
-
-                const data_param = { testData: "test value" }
-
-                componentInstance.raiseToErrorOverlay(data_param);
-
-                expect(ExceptionsHandler.ValidatorError).toHaveBeenCalledWith("view-103");
-            });
-        })
-        
-        describe("Case 3: When the \"data\" parameter is an object, and the onRaiseToErrorOverlay props IS a function", () => {
-            test("Run raiseToErrorOverlay({ testData: \"test value\" }) when this.props.onRaiseToErrorOverlay is a function: Call this.props.onRaseToErrorOverlay(data) using the same \"data\" parameter", () => {
-                jest.useFakeTimers();
-
-                const presetProps = {
-                    onRaiseToErrorOverlay: jest.fn()
-                };
-                testComponent = predefinedComponent(presetProps, { disableLifecycleMethods: true });
-                componentInstance = testComponent.instance();
-
-                const data_param = { testData: "test value" }
-
-                componentInstance.raiseToErrorOverlay(data_param);
-                jest.runAllTimers();
-
-                expect(componentInstance.props.onRaiseToErrorOverlay).toHaveBeenCalledWith(data_param);
-                jest.useRealTimers();
-            });
-        });
+        expect(componentInstance.handleViewMount).toHaveBeenCalled();
+      });
     });
+  });
 
-    describe("Test handleViewMount()", () => {
-        const various_props_onViewMount = [
-            ["A string representing a dummy data variable"],
-            [32],
-            [null],
-            [undefined],
-            [false],
-            [true],
-            [[12,8,3,7]],
-            [{ item: "test" }]
-        ];
-
-        test.each(various_props_onViewMount)("Run handleViewMount(), when this.props.onViewMount = %p: Throw an error ExceptionsHandler.ValidatorError(\"view-101\")", (val) => {
-            const presetProps = {
-                onViewMount: val
-            }
-            testComponent = predefinedComponent(presetProps, { disableLifecycleMethods: true });
-            componentInstance = testComponent.instance();
-
-            componentInstance.handleViewMount();
-            expect(ExceptionsHandler.ValidatorError).toHaveBeenCalledWith("view-101");
-        });
-
-        test("Run handleViewMount(), when this.props.onViewMount is a function: Call this.props.onViewMount() with the component state as input parameter", () => {
-            const presetProps = {
-                onViewMount: jest.fn()
-            }
-            testComponent = predefinedComponent(presetProps, { disableLifecycleMethods: true });
-            componentInstance = testComponent.instance();
-
-            componentInstance.handleViewMount();
-            expect(componentInstance.props.onViewMount).toHaveBeenCalledWith(componentInstance.state);
-        })
+  describe("Test render()", () => {
+    test('Run render(): Nothing should be rendered at all, no JSX. Nothing. Just return "null"', () => {
+      expect(componentInstance.render()).toBeNull();
     });
-
-    describe("Test raiseToModal(data)", () => {
-        describe("Case 1: When the \"data\" parameter is NOT an object", () => {
-            const various_data = [
-                ["A string representing a dummy data variable"],
-                [32],
-                [null],
-                [undefined],
-                [false],
-                [true],
-                [[12,8,3,7]],
-                [() => {}]
-            ];
-
-            test("Run raiseToModal(): Throw an error ExceptionsHandler.ValidatorError(\"view-104\")", () => {
-                componentInstance.raiseToModal();
-
-                expect(ExceptionsHandler.ValidatorError).toHaveBeenCalledWith("view-104");
-            });
-
-            test.each(various_data)("Run raiseToModal(%p): Throw an error ExceptionsHandler.ValidatorError(\"view-104\")", (val) => {
-                componentInstance.raiseToModal(val);
-
-                expect(ExceptionsHandler.ValidatorError).toHaveBeenCalledWith("view-104");
-            });
-        });
-        
-        describe("Case 2: When the \"data\" parameter is an object, but the onRaiseToModal props is NOT a function", () => {
-            const various_props_onRaiseToModal = [
-                ["A string representing a dummy data variable"],
-                [32],
-                [null],
-                [undefined],
-                [false],
-                [true],
-                [[12,8,3,7]],
-                [{ testItem: "test" }]
-            ];
-    
-            test("Run raiseToModal({ testData: \"test value\" }), while this.props.onRaiseToModal does not exist: Throw an error ExceptionsHandler.ValidatorError(\"view-105\")", () => {
-                const presetProps = {
-                  
-                }
-                testComponent = predefinedComponent(presetProps, { disableLifecycleMethods: true });
-                componentInstance = testComponent.instance();
-    
-                const data_param = { testData: "test value" };
-                componentInstance.raiseToModal(data_param);
-    
-                expect(ExceptionsHandler.ValidatorError).toHaveBeenCalledWith("view-105");
-            });
-    
-            test.each(various_props_onRaiseToModal)("Run raiseToModal({ testData: \"test value\" }), while this.props.onRaiseToModal does not exist: Throw an error ExceptionsHandler.ValidatorError(\"view-105\")", (val) => {
-                const presetProps = {
-                    onRaiseToModal: val
-                }
-                testComponent = predefinedComponent(presetProps, { disableLifecycleMethods: true });
-                componentInstance = testComponent.instance();
-    
-                const data_param = { testData: "test value" };
-                componentInstance.raiseToModal(data_param);
-    
-                expect(ExceptionsHandler.ValidatorError).toHaveBeenCalledWith("view-105");
-            })
-        });
-    
-        describe("Case 3: When the \"data\" parameter is an object, and the onRaiseToModal props is a function", () => {
-            test("Run raiseToModal({ testData: \"test value\" }) when this.props.onRaiseToModal is a function: Call this.props.onRaseToModal(data) using the same \"data\" parameter", () => {
-
-                const presetProps = {
-                    onRaiseToModal: jest.fn()
-                };
-                testComponent = predefinedComponent(presetProps, { disableLifecycleMethods: true });
-                componentInstance = testComponent.instance();
-    
-                const data_param = { testData: "test value" }
-    
-                componentInstance.raiseToModal(data_param);
-
-                expect(componentInstance.props.onRaiseToModal).toHaveBeenCalledWith(data_param);
-
-            });
-        });
-    
-    });
-
-    describe("Test componentDidMount()", () => {
-        test("Run componentDidMount, when this.childComponentDidMount is a function: Call this.childComponentDidMount()", () => {
-            componentInstance.handleViewMount = jest.fn();
-            componentInstance.childComponentDidMount = jest.fn();
-            componentInstance.componentDidMount();
-
-            expect(componentInstance.childComponentDidMount).toHaveBeenCalled();
-        });
-    })
-
-    describe("Test render()", () => {
-        test("Run render(): Nothing should be rendered at all, no JSX. Nothing. Just return \"null\"", () => {
-            expect(componentInstance.render()).toBeNull();
-        })
-    })
-}) 
+  });
+});
