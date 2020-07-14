95e3dd207f4e2c363dae5864f3ed39b1257d5de2
Njugen <privat_thai_nguyen@hotmail.com>
2020-07-14 20:34:56 +0300

    updated .gitignore

--- src/components/modals/currentlyOpenedTabsModule/cotmRemoveUnresponsiveTabsModal/BodyContents.jsx
+++
@@ -1,7 +0,0 @@
-import React, { Component } from "react";
-
-export default class BodyContents extends Component {
-  render() {
-    return <p>Go through all opened tabs and remove them from the list</p>;
-  }
-}
--- src/components/modals/currentlyOpenedTabsModule/cotmRemoveUnresponsiveTabsModal/HeaderContents.jsx
+++
@@ -1,7 +0,0 @@
-import { Component } from "react";
-
-export default class HeaderContents extends Component {
-  render() {
-    return "Close unresponsive tabs";
-  }
-}
