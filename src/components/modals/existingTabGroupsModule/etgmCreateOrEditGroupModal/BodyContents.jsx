ca3ce3aea37936f7753a1a4cc04ba5f1b461613b
Njugen <privat_thai_nguyen@hotmail.com>
2020-07-16 01:04:47 +0300

    Updated readme.md

--- src/components/modals/existingTabGroupsModule/etgmCreateOrEditGroupModal/BodyContents.jsx
+++ src/components/modals/existingTabGroupsModule/etgmCreateOrEditGroupModal/BodyContents.jsx
@@ -8,8 +8,82 @@ import { ValidatorError } from "./../../../utils/exceptionsAndHandler";
 import TBScheduleListInput from "../../../utils/form/tbScheduleListInput";
 
 export default class BodyContents extends Component {
+  verifyProps = () => {
+    const {
+      isObject,
+      isFunction,
+      isUndefined,
+      isString,
+      isBoolean,
+    } = validator;
+
+    const { data, fieldErrors, onChange, tabGroupDetails } = this.props;
+
+    if (!isObject(data))
+      throw ValidatorError("etgmCreateOrEditGroupModal-err-1");
+    if (!isObject(fieldErrors))
+      throw ValidatorError("etgmCreateOrEditGroupModal-err-2");
+    if (!isFunction(onChange))
+      throw ValidatorError("etgmCreateOrEditGroupModal-err-3");
+    if (!isObject(tabGroupDetails))
+      throw ValidatorError("etgmCreateOrEditGroupModal-err-4");
+
+    if (isObject(data)) {
+      const { params } = data;
+
+      if (!isObject(params))
+        throw ValidatorError("etgmCreateOrEditGroupModal-err-5");
+
+      const {
+        groupName,
+        groupCloseAll,
+        groupCloseInactiveTabs,
+        groupDescription,
+        groupDontAskAgain,
+        type,
+      } = params;
+
+      // If there is a groupName variable, it has to be a string (groupName is optional)
+      if (!isUndefined(groupName) && !isString(groupName))
+        throw ValidatorError("etgmCreateOrEditGroupModal-err-6");
+
+      // If there is a groupCloseAll variable, it has to be a bool (groupCloseAll is optional)
+      if (!isUndefined(groupCloseAll) && !isBoolean(groupCloseAll))
+        throw ValidatorError("etgmCreateOrEditGroupModal-err-7");
+
+      // If there is a groupCloseInactiveTabs variable, it has to be a bool (groupCloseInactiveTabs is optional)
+      if (
+        !isUndefined(groupCloseInactiveTabs) &&
+        !isBoolean(groupCloseInactiveTabs)
+      )
+        throw ValidatorError("etgmCreateOrEditGroupModal-err-8");
+
+      // If there is a groupDescription variable, it has to be a string (groupDescription is optional)
+      if (!isUndefined(groupDescription) && !isString(groupDescription))
+        throw ValidatorError("etgmCreateOrEditGroupModal-err-9");
+
+      // If there is a groupDontAskAgain variable, it has to be a bool (groupDontAskAgain is optional)
+      if (!isUndefined(groupDontAskAgain) && !isBoolean(groupDontAskAgain))
+        throw ValidatorError("etgmCreateOrEditGroupModal-err-10");
+
+      // If there is a type variable, it has to be a string (type is optional)
+      if (!isUndefined(type) && !isString(type))
+        throw ValidatorError("etgmCreateOrEditGroupModal-err-11");
+      if (
+        type !== "currently-opened" &&
+        type !== "existing-group" &&
+        type !== "new-group"
+      )
+        throw ValidatorError("etgmCreateOrEditGroupModal-err-12");
+    }
+  };
+
+  componentDidUpdate = () => {
+    this.verifyProps();
+  };
+
   render() {
-    const { isObject, isArray } = validator;
+    const { isObject } = validator;
     const { data, fieldErrors, onChange, tabGroupDetails } = this.props;
 
     if (isObject(data)) {
